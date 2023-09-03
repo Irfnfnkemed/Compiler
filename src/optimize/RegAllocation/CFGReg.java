b002af41bd42d305cbdf758a4b5a566a4480cdfc
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-09-03 11:48:40 +0800

    fix the bug: asmInstr useNumber not initialize

--- src/optimize/RegAllocation/CFGReg.java
+++ src/optimize/RegAllocation/CFGReg.java
@@ -68,6 +68,7 @@ public class CFGReg {
                     }
                     instr.def = ((LI) instr).to;
                 } else if (instr instanceof LW) {
+                    instr.useNum = 0;
                     if (((LW) instr).offset != -1 && !((LW) instr).from.contains("#")) {
                         nowBlock.blockLive.addUse(((LW) instr).from);
                         instr.use[0] = ((LW) instr).from;
@@ -79,6 +80,7 @@ public class CFGReg {
                     nowBlock.blockLive.addDef(((LA) instr).to);
                     instr.def = ((LA) instr).to;
                 } else if (instr instanceof SW) {
+                    instr.useNum = 0;
                     if (!Objects.equals(((SW) instr).from, "zero")) {
                         nowBlock.blockLive.addUse(((SW) instr).from);
                         instr.use[instr.useNum++] = ((SW) instr).from;
@@ -88,6 +90,7 @@ public class CFGReg {
                         instr.use[instr.useNum++] = ((SW) instr).to;
                     }
                 } else if (instr instanceof MV) {
+                    instr.useNum = 0;
                     if (!Objects.equals(((MV) instr).from, "zero")) {
                         if (instr.preColoredFrom == null) {
                             nowBlock.blockLive.addUse(((MV) instr).from);
@@ -99,6 +102,7 @@ public class CFGReg {
                     }
                     instr.def = ((MV) instr).to;
                 } else if (instr instanceof binBase) {
+                    instr.useNum = 0;
                     if (!Objects.equals(((binBase) instr).lhs, "zero")) {
                         nowBlock.blockLive.addUse(((binBase) instr).lhs);
                         instr.use[instr.useNum++] = ((binBase) instr).lhs;
@@ -110,27 +114,28 @@ public class CFGReg {
                     nowBlock.blockLive.addDef(((binBase) instr).to);
                     instr.def = ((binBase) instr).to;
                 } else if (instr instanceof binImmeBase) {
+                    instr.useNum = 0;
                     nowBlock.blockLive.addUse(((binImmeBase) instr).from);
                     nowBlock.blockLive.addDef(((binImmeBase) instr).to);
                     instr.use[0] = ((binImmeBase) instr).from;
                     instr.def = ((binImmeBase) instr).to;
                     instr.useNum = 1;
                 } else if (instr instanceof SEQZ) {
+                    instr.useNum = 0;
                     nowBlock.blockLive.addUse(((SEQZ) instr).from);
                     nowBlock.blockLive.addDef(((SEQZ) instr).to);
-                    instr.use[0] = ((SEQZ) instr).from;
+                    instr.use[instr.useNum++] = ((SEQZ) instr).from;
                     instr.def = ((SEQZ) instr).to;
-                    instr.useNum = 1;
                 } else if (instr instanceof SNEZ) {
+                    instr.useNum = 0;
                     nowBlock.blockLive.addUse(((SNEZ) instr).from);
                     nowBlock.blockLive.addDef(((SNEZ) instr).to);
-                    instr.use[0] = ((SNEZ) instr).from;
+                    instr.use[instr.useNum++] = ((SNEZ) instr).from;
                     instr.def = ((SNEZ) instr).to;
-                    instr.useNum = 1;
                 } else if (instr instanceof BNEZ) {
+                    instr.useNum = 0;
                     nowBlock.blockLive.addUse(((BNEZ) instr).condition);
-                    instr.use[0] = ((BNEZ) instr).condition;
-                    instr.useNum = 1;
+                    instr.use[instr.useNum++] = ((BNEZ) instr).condition;
                 }
             }
             nowBlock.blockLive.liveIn.addAll(nowBlock.blockLive.blockUse);
