85a42853f950ca73ddd15a28801860aaa4901a6d
Irfnfnkemed <Irfnfnkemed@sjtu.edu.cn>
2023-09-08 00:34:09 +0800

    change para

--- src/optimize/RegAllocation/RegAllocation.java
+++ src/optimize/RegAllocation/RegAllocation.java
@@ -34,7 +34,7 @@ public class RegAllocation {
             var list = ASMInstrMap.get(funcName);
             ASMInstrMap.remove(funcName);
             Function function = new Function(list, asmBuilder.globalVar);
-            if (function.graphColor.used.size() < 10 && function.asmInstrList.size() < 1000) {
+            if (function.graphColor.used.size() < 10 && function.asmInstrList.size() < 100) {
                 var funcNode = asmBuilder.getNode(funcName);
                 changeParaAndRet((Init) list.get(1));
                 for (CALL call : funcNode.callList) {
